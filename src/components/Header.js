98939a6e13d19cd7fe46e71db4595032f79326cd
prachimote98 <118653542+prachimote98@users.noreply.github.com>
2022-11-20 13:22:39 +0530

    food recipee

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+const Header=()=>{
+    return(
+        <div className="jumbtron">
+            <h1 className="display-1"><i class="material-icons">
+                fastfood
+                </i>Food Recipe</h1>
+        </div>
+    )
+}
+export default Header;
---
+++ src/components/Recipe.js
@@ -0,0 +1,9 @@
+import React from"react";
+
+const Recipe=()=>{
+    return(
+        <h1>All Recipe</h1>
+    )
+}
+
+export default Recipe;
